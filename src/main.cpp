b2e0c8b18f88ed65b4fba6869ec6610fdd24e5cf
DeathSpreads <68755892+SniffyMcTasty@users.noreply.github.com>
2023-03-07 11:32:44 -0500

    Refactored ImGui dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 #include "Text.h"
 
 #include "ImGui/imgui.h"
+#include "ImGui/imgui_impl_opengl3.h"
+#include "ImGui/imgui_impl_glfw.h"
 
 using namespace TAPP;
 using namespace std;
@@ -31,7 +33,7 @@ int main(int argc, char* argv[]){
         return -1;
     }
     
-    Virtual3DLayer* view = new Virtual3DLayer(w, h);
+    Virtual3DLayer* view = new Virtual3DLayer(512, 384);
     gapp.m_window.m_layers.push_back(view);
     
     RenderModel* sp = new RenderModel(argv[1]);
@@ -50,10 +52,20 @@ int main(int argc, char* argv[]){
     text->m_size = 60;
     view->m_objects.push_back(text);
  */   
+
+    // Setup ImGui
+    // IMGUI_CHECKVERSION();
+    // ImGui::CreateContext();
+    // ImGuiIO& io = ImGui::GetIO();
+    // ImGui_ImplGlfw_InitForOpenGL(gapp.m_window.m_window, true);
+    // ImGui_ImplOpenGL3_Init();
+    // ImGui::StyleColorsDark();
  
     gapp.run();
     
+    //ImGui::DestroyContext();
     gapp.release();
+
 
 	return 0;
 }

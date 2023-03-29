59ddb59fc945fb6badf7cfa7cd84181c6f352a10
DeathSpreads <68755892+SniffyMcTasty@users.noreply.github.com>
2023-03-29 16:28:06 -0400

    Implement balloon shader

--- assets/Balloon.fragmentshader.glsl
+++ assets/Balloon.fragmentshader.glsl
@@ -1,7 +1,40 @@
-precision mediump float;
-uniform sampler2D u_sampler0;
-varying vec2 v_tex_coord0;
+#version 330 core
 
-void main(void) {
-    gl_FragColor = texture2D(u_sampler0, v_tex_coord0);
+// Interpolated values from the vertex shaders
+in vec3 Position_worldspace;
+in vec3 Normal_cameraspace;
+in vec3 EyeDirection_cameraspace;
+in vec3 LightDirection_cameraspace;
+
+// Ouput data
+out vec3 color;
+
+// Values that stay constant for the whole mesh.
+uniform mat4 MV;
+uniform vec3 LightPosition_worldspace;
+uniform vec3 diffuse_color;
+
+void main() {
+
+    // Compute the intensity
+    vec3 lightDirection = normalize(LightPosition_worldspace - Position_worldspace);
+    float intensity = dot(Normal_cameraspace, lightDirection);
+
+    // Clamp intensity from 0 to 1
+    intensity = clamp(intensity, 0, 1);
+
+    // Intensity is the same for all colors
+    vec3 result = diffuse_color * intensity;
+
+    // Add some ambient
+    result += vec3(0.1, 0.1, 0.1);
+
+    // Send the color to the GPU
+    color = result;
+
+    // Add some specular
+    float specular = dot(Normal_cameraspace, normalize(LightDirection_cameraspace + EyeDirection_cameraspace));
+    specular = clamp(specular, 0, 1);
+    specular = pow(specular, 4);
+    color += vec3(0.5, 0.5, 0.5) * specular;
 }
--- assets/Balloon.vertexshader.glsl
+++ assets/Balloon.vertexshader.glsl
@@ -1,33 +1,44 @@
-attribute vec4 a_position;
-attribute vec2 a_tex_coord0;
-varying vec2 v_tex_coord0;
+#version 330 core
 
-uniform float uf_time;
-uniform mat4 um4_matrix;
-uniform mat4 um4_pmatrix;
-const float cf_divisor = 16.0;
+// Input vertex data, different for all executions of this shader.
+layout(location = 0) in vec3 vertexPosition_modelspace;
+layout(location = 1) in vec3 vertexNormal_modelspace;
 
-void main(void) {
-    vec4 pos = a_position;
-    float fX = abs(pos.x);
-    float fY = abs(pos.y);
+// Output data ; will be interpolated for each fragment.
+out vec3 Position_worldspace;
+out vec3 Normal_cameraspace;
+out vec3 EyeDirection_cameraspace;
+out vec3 LightDirection_cameraspace;
 
-    // Move X and Y coordinates
-    // in toward the center.
-    // Apply to corner vertices
-    // only.
-    if (fX == 1.0 && fY == 1.0)
-    {
-        float f_offset = 1.0 - (uf_time/cf_divisor);
-        pos.x *= f_offset;
-        pos.y *= f_offset;
-    }
-    // Move center vertices
-    // toward the viewport.
-    else if (fX == 0.0 && fY == 0.0)
-    {
-        pos.z = uf_time;
-    }
-    gl_Position = um4_pmatrix * um4_matrix * pos;
-    v_tex_coord0 =  a_tex_coord0;
+// Values that stay constant for the whole mesh.
+uniform mat4 MVP;
+uniform mat4 V;
+uniform vec3 LightPosition_worldspace;
+
+// Balloon vertex shader main function
+void main(){
+
+    // Apply balloon-like transformation to the vertex position
+    vec3 vertexPosition = vertexPosition_modelspace;
+    vertexPosition.x = vertexPosition.x + vertexNormal_modelspace.x * 0.1;
+    vertexPosition.y = vertexPosition.y + vertexNormal_modelspace.y * 0.1;
+    vertexPosition.z = vertexPosition.z + vertexNormal_modelspace.z * 0.1;
+
+    // Output position of the vertex, in clip space : MVP * position
+    gl_Position =  MVP * vec4(vertexPosition,1);
+
+    // Position of the vertex, in worldspace : M * position
+    vec4 vertexPosition_worldspace = V * vec4(vertexPosition,1);
+    Position_worldspace = vertexPosition_worldspace.xyz;
+
+    // Vector that goes from the vertex to the light, in worldspace : MV * position - lightposition
+    vec3 LightPosition_cameraspace = (V * vec4(LightPosition_worldspace,1)).xyz;
+    LightDirection_cameraspace = LightPosition_cameraspace - Position_worldspace;
+
+    // Vector that goes from the vertex to the camera, in worldspace.
+    // In camera space, the camera is at the origin (0,0,0).
+    EyeDirection_cameraspace = vec3(0,0,0) - Position_worldspace;
+
+    // Normal of the vertex, in camera space.
+    Normal_cameraspace = mat3(transpose(inverse(V))) * vertexNormal_modelspace;
 }
